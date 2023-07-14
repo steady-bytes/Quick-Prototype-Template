5848536c5805f1e756504ec920a68133f6c5eb8c
andrewsc208 <meilingandrew@gmail.com>
2023-07-14 11:02:29 -0500

    chore: Still working on user signup, and then app rendering

--- server/src/handler/signup.rs
+++ server/src/handler/signup.rs
@@ -2,7 +2,7 @@ use serde::Deserialize;
 use sqlx::postgres::PgPool;
 
 use axum::{
-    extract::Path,
+    extract::{Path, Query},
     Extension,
     response::{Html, IntoResponse, Redirect},
     Form,
@@ -31,18 +31,35 @@ pub async fn greet(
     Html(templates.render("hello", &context).unwrap())
 }
 
+#[derive(Deserialize)]
+pub struct SignupErrorParams {
+    pub error: Option<String>,
+}
+
+#[axum_macros::debug_handler]
 pub async fn render_signup_page(
+    params: Query<SignupErrorParams>,
     Extension(templates): Extension<templates::Templates>,
+    Extension(pool): Extension<PgPool>,
     session: Session<SessionPgPool>,
 ) -> impl IntoResponse {
     let mut context = templates::new_template_context();
     context.insert("authenticity_token", "sfsdfs");
+    context.insert("error", &params.error);
 
     let mut count : usize = session.get("count").unwrap_or(0);
     count += 1;
     session.set("count", count);
 
-    println!("{}", count.to_string());
+    match sqlx::query!("SELECT COUNT(*) FROM users").fetch_all(&pool).await {
+        Ok(v) => {
+            match v[0].count {
+                Some(0) => context.insert("admin", &true),
+                _ => context.insert("admin", &false),
+            }
+        },
+        Err(_e) => context.insert("admin", &false)
+    }
 
     Html(templates.render("signup_page", &context).unwrap())
 }
@@ -53,6 +70,7 @@ pub struct NewUserRequest {
     password: String,
     confirm_password: String,
     offline: bool,
+    admin: Option<bool>,
 }
 
 pub async fn signup_user(
@@ -62,17 +80,12 @@ pub async fn signup_user(
 ) -> Redirect { 
     let mut count : usize = session.get("count").unwrap_or(0);
     count += 1;
-    session.set("count", count);
-    println!("{}", count.to_string());
-    println!("offline value {}", &req.offline);
+    session.set("count", count); 
 
-    // validate input
     if &req.password != &req.confirm_password {
-        println!("passwords don't match");
-        return Redirect::to("/signup?error=password_match_error")
-    } else if &req.password.len() < &8 {
-        println!("password to short");
-        return Redirect::to("/signup?error=password_length")
+        return Redirect::to("/signup?error=password_match")
+    } else if &req.password.len() < &8 || &req.password.len() > &20 {
+        return Redirect::to("/signup?error=password_strength")
     }
 
     match sqlx::query("INSERT INTO users (email, password) values ($1, $2)")
@@ -80,8 +93,7 @@ pub async fn signup_user(
         .bind(&req.password)
         .execute(&pool)
         .await {
-            Ok(row) => {
-                println!("user: {:?}", row);
+            Ok(_row) => {
                 // generate access, refresh tokens with the role (default, admin)
                 let mut token_options = jwt::ForgeOptions{ offline_mode: false};
                 if req.offline {
@@ -90,7 +102,6 @@ pub async fn signup_user(
 
                 match jwt::forge_tokens(&req.email, Some(token_options)) {
                     Ok(tokens) => {
-                        println!("tokens result: {:?}", tokens);
                         // add access token to session
                         session.set("access_token", tokens.access_token);
                         session.set("id_token", tokens.id_token);
@@ -100,9 +111,12 @@ pub async fn signup_user(
                         println!("and error occurred when forging the tokens {:?}", e);
                     }
                 }
+
+                // TODO: Change insert to a transaction, if it's an admin setup then
+                // add the admin role, else add the default role
+
             },
-            Err(e) => {
-                println!("error: {:?}", e); 
+            Err(_e) => {
                 // redirect back to the signup page with an error
                 return Redirect::to("/signup?error=unique_email")
             }
